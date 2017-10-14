46369e3107a61c2546908dc4a9265a0780fb5a0e
renan <renan@aestesis.org>
2017-10-14 04:48:06 +0200

    tags

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "Aether-example",targets: ["Aether-example"])
     ],
     dependencies: [
-        .package(url: "https://github.com/aestesis/Aether.git", from:"0.0.6")
+        .package(url: "https://github.com/aestesis/Aether.git", from:"0.0.8")
     ],
     targets: [
         .target(name: "Aether-example",dependencies: ["Aether"]),
