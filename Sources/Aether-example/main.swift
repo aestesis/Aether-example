b137856af38988c4b673aeb3d2e34348188b3004
renan <renan@aestesis.org>
2017-10-17 22:01:56 +0200

    in progress

--- Sources/Aether-example/main.swift
+++ Sources/Aether-example/main.swift
@@ -1,15 +1,13 @@
 import Foundation
 import Aether
 
-Debug.warning(Application.name)
-Debug.warning(Application.version)
-Debug.warning(Application.build)
+Debug.warning("\(FileManager.default.homeDirectoryForCurrentUser.path)")
 
 let window = OsWindow(title:"Aether Example",width:400,height:300)
 if let window = window {
     window.onStartUI.once { viewport in
-        //let home = Home(viewport:viewport)
-        //viewport.rootView = home
+        let home = Home(viewport:viewport)
+        viewport.rootView = home
     }
     window.renderLoop()
 }
