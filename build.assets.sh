01762080516d373ef65f62c657a93e39ec5360dc
renan <renan@aestesis.org>
2017-10-21 15:08:47 +0200

    added assets packager

---
+++ build.assets.sh
@@ -0,0 +1,8 @@
+# no need to do it, except to update changes
+
+cd Packages/Aether
+./build.assets.sh
+cd ../..
+mv Packages/Aether/Aether.assets.zip assets.zip
+
+# merge 2 zip https://superuser.com/questions/92963/how-to-combine-merge-zip-files
