97af718095c791cb52f4ea8aca8b31acb2b07a07
renan <renan@aestesis.org>
2017-10-23 00:21:37 +0200

    ...

--- Sources/Aether-example/main.swift
+++ Sources/Aether-example/main.swift
@@ -4,6 +4,24 @@ import Aether
 Application.name = "Aether-example"
 Debug.codeRoot = "Aether-example"
 
+func scanAssets() {
+    let fm = FileManager.default
+    var path:String? = fm.currentDirectoryPath 
+    while path != nil {
+        NSLog("**** scan \(path)")
+        do {
+            let files = try fm.contentsOfDirectory(atPath: path!)
+            for f in files {
+                NSLog("\(f)")
+            }
+        } catch _ {
+        }
+        path = path?.parentPath 
+    }
+}
+
+scanAssets()
+/*
 let window = OsWindow(title:"Aether Example",width:400,height:300)
 if let window = window {
     window.onStartUI.once { viewport in
@@ -12,3 +30,4 @@ if let window = window {
     }
     window.renderLoop()
 }
+*/
--- build.assets.sh
+++ build.assets.sh
@@ -3,6 +3,6 @@
 cd Packages/Aether
 ./build.assets.sh
 cd ../..
-mv Packages/Aether/Aether.assets.zip assets.zip
+mv Packages/Aether/assets.aether.zip assets.aether.zip
 
 # merge 2 zip https://superuser.com/questions/92963/how-to-combine-merge-zip-files
