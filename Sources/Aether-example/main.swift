99f69764962124d31a5472d6cf6a927c5ebf7cff
renan <renan@aestesis.org>
2017-10-24 05:39:45 +0200

    in progress

--- Sources/Aether-example/main.swift
+++ Sources/Aether-example/main.swift
@@ -1,26 +1,29 @@
 import Foundation
 import Aether
 
-Application.name = "Aether-example"
-Debug.codeRoot = "Aether-example"
 
-func scanAssets() {
-    let fm = FileManager.default
-    var path:String? = fm.currentDirectoryPath 
-    while path != nil {
-        NSLog("**** scan \(path)")
-        do {
-            let files = try fm.contentsOfDirectory(atPath: path!)
-            for f in files {
-                NSLog("\(f)")
-            }
-        } catch _ {
-        }
-        path = path?.parentPath 
+Application.initialize(name:"Aether-example",codeRoot:"Aether-example",assets:["assets.aether.zip"])
+
+Application.getText("LICENSE") { r in
+    Debug.warning("yo!!!")
+    if let s = r as? String {
+        Debug.warning(s)
     }
 }
 
-scanAssets()
+
+
+if let f = Application.readFile("LICENSE") {
+    NSLog("available: \(f.available)")
+    if let data = f.read(f.available) {
+        NSLog("data: \(data.count)")
+    } else {
+        NSLog("no data")
+    }
+} else {
+    NSLog("no file")
+}
+
 /*
 let window = OsWindow(title:"Aether Example",width:400,height:300)
 if let window = window {
