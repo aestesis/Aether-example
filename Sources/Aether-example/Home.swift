fb8f12b4f3c64c2f05cb6cc37aaf7e0ce2b46f2d
renan <renan@aestesis.org>
2018-03-10 17:19:36 +0100

    debugging

--- Sources/Aether-example/Home.swift
+++ Sources/Aether-example/Home.swift
@@ -1,6 +1,10 @@
 import Aether
 
 class Home:View {
+    public override init(viewport:Viewport) {
+        super.init(viewport:viewport)
+        self.background = .white    // 4debug
+    }
     override func draw(to g:Graphics) {
         g.fill(rect:self.bounds,color:.aeOrange)
     }
